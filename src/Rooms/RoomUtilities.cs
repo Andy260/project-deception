d1620eccfdacb8c21af90b426eb715d5a0f4dfee
Andrew Barbour <andrew.barbour1@gmail.com>
2022-12-28 00:22:19 +1100

    Implemented RoomUtitlities helper class for utilities relating to rooms

---
+++ src/Rooms/RoomUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProjectDeception.Rooms
+{
+    /// <summary>
+    /// Helper class for various methods
+    /// assisting with Rooms
+    /// </summary>
+    public static partial class RoomUtilities
+    {
+        // Regular expression for
+        // testing generated room codes
+        [GeneratedRegex("[A-Z, 0-9]{6}")]
+        private static partial Regex RoomCodeRegex();
+
+        /// <summary>
+        /// Is the given room code valid?
+        /// </summary>
+        /// <param name="roomCode">Room code to check</param>
+        /// <returns>True if the given room code is valid</returns>
+        public static bool IsValidRoomCode(string roomCode)
+        {
+            if (string.IsNullOrWhiteSpace(roomCode) || roomCode.Length > 6)
+            {
+                return false;
+            }
+
+            return RoomCodeRegex().IsMatch(roomCode);
+        }
+    }
+}
---
+++ tests/RoomUtilitiesTests.cs
@@ -0,0 +1,43 @@
+﻿using ProjectDeception.Rooms;
+using NUnit.Framework;
+
+namespace ProjectDeception.Tests
+{
+    /// <summary>
+    /// Test suite for <see cref="RoomUtilities"/>
+    /// </summary>
+    [TestFixture]
+    public class RoomUtilitiesTests
+    {
+        #region Public Function Tests
+
+        [Test]
+        [TestCase("9ANM01")]
+        [TestCase("RM5N17")]
+        [TestCase("QL7N9A")]
+        public void IsValidRoomCode(string roomCode)
+        {
+            // Act & Assert
+            Assert.That(RoomUtilities.IsValidRoomCode(roomCode), Is.True);
+        }
+
+        [Test]
+        public void IsValidRoomCode_Whitespace()
+        {
+            // Act & Assert
+            Assert.That(RoomUtilities.IsValidRoomCode(" "), Is.False);
+        }
+
+        [Test]
+        [TestCase("dsfsv01")]
+        [TestCase("JKAL0915")]
+        [TestCase("u6qe57")]
+        public void IsValidRoomCode_InvalidCode(string roomCode)
+        {
+            // Act & Assert
+            Assert.That(RoomUtilities.IsValidRoomCode(roomCode), Is.False);
+        }
+
+        #endregion
+    }
+}
